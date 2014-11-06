70c75480b3ed21763dbdd4f9aa4e2704351f817c
mooveprince2@gmail.com <mooveprince2@gmail.com>
2014-11-06 11:55:52 -0600

    working

--- index.js
+++ index.js
@@ -5,30 +5,42 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-    console.log ("Inside of Scrape..");
-    //url = "https://www.xoom.com/india/send-money";
-    url = 'http://www.imdb.com/title/tt1229340/';
-    var output = { country : "", rate : ""};    
-    request(url, function(error, response, html){
-        if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-            var $ = cheerio.load(html);
-            // Finally, we'll define the variables we're going to capture
-            var country, rate;
-            console.log ("After loading Xoom..");
-           $('.header').filter(function(){
-                console.log ("After fetching data..");
-                output.rate = $(this).children().first().text();
-            });
-            res.send ("Value is.." + output.rate);
-            } else {
-                console.log ("Error occurred..."+ error);
-            }
-    });    
-    res.end("Scrape Info.." + output);
-});    
+	
+	url = 'https://www.xoom.com/india/send-money';
+
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = { rate : ""};
+
+			$('.fx-rate').filter(function(){
+		        rate = $(this).first().text();           
+		        json.rate = rate;
+	        })
+
+		} else {
+            console.log ("Error after making request to URL");
+        }
+        
+        // To write to the system we will use the built in 'fs' library.
+        // In this example we will pass 3 parameters to the writeFile function
+        // Parameter 1 :  output.json - this is what the created filename will be called
+        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+        	console.log('output.json file');
+
+        })
+
+        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+        res.send('Check your console!')
+	})
+})
 
 app.listen('3000')
-
-
+console.log('Port on 3000');
 exports = module.exports = app;
